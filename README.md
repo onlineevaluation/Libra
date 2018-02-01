129263bb403e24fd67da9a5300eb86fac492ccbd
youngxhui <youngxhui@gmail.com>
2018-02-01 11:42:56 +0800

    add cors support

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # evaluate
 
 [![Build Status](https://travis-ci.org/youngxhui/evaluate.svg?branch=master)](https://travis-ci.org/youngxhui/evaluate)
+
+# 主要功能
+
---
+++ src/main/kotlin/com/nuc/evaluate/config/CorsConfig.kt
@@ -0,0 +1,21 @@
+package com.nuc.evaluate.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+/**
+ * @author 杨晓辉 2018/2/1 11:41
+ * 全局跨域请求支持
+ */
+@Configuration
+class CorsConfig : WebMvcConfigurerAdapter() {
+
+    override fun addCorsMappings(registry: CorsRegistry?) {
+        registry!!.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(false)
+                .maxAge(3600)
+    }
+}
