569095c7f23a0e6d074cf56e00773f74948b4732
youngxhui <youngxhui@gmail.com>
2018-02-01 10:04:33 +0800

    Initial commit

---
+++ src/main/kotlin/com/nuc/evaluate/EvaluateApplication.kt
@@ -0,0 +1,11 @@
+package com.nuc.evaluate
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class EvaluateApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(EvaluateApplication::class.java, *args)
+}
