23334a7f4b595579e68cd218e26286d96a4be97a
youngxhui <youngxhui@gmail.com>
2018-12-29 17:01:50 +0800

    build(sql.ci): 修复ci构建
    - 添加ci构建的数据内容
    - 完善UserLogin测试类

--- ci.sql
+++ ci.sql
@@ -2,6 +2,8 @@ CREATE DATABASE IF NOT EXISTS eva;
 
 use eva;
 
+-- 创建用户表
+
 create table uek_privilege_user
 (
 	id int auto_increment
@@ -11,5 +13,45 @@ create table uek_privilege_user
 	status tinyint not null
 );
 
+-- 插入用户数据
+
 INSERT INTO eva.uek_privilege_user (id, username, password, status) VALUES (1, 'admin', '96e79218965eb72c92a549dd5a330112', 2);
 INSERT INTO eva.uek_privilege_user (id, username, password, status) VALUES (811, '1713010101', '96e79218965eb72c92a549dd5a330112', 1);
+
+-- 创建学生表
+
+create table uek_acdemic_students
+(
+	id int auto_increment
+		primary key,
+	name varchar(50) null,
+	student_number varchar(225) null,
+	pro_team_id int default 0 null comment '项目组id',
+	status tinyint default 2 not null comment '0 禁用  1 计划学员 2 正式学员 ',
+	gender tinyint null comment '0女 1男',
+	nation varchar(50) charset gbk null comment '民族',
+	phone varchar(20) charset gbk null,
+	qq varchar(20) charset gbk null,
+	email varchar(50) charset gbk null,
+	idcard varchar(50) charset gbk null,
+	member_id int null,
+	user_id int null,
+	class_id int null
+)
+comment '学生表';
+
+create index t_stu_pro_team_fk
+	on uek_acdemic_students (pro_team_id)
+	comment '(null)';
+
+create index tg_class_student_fk
+	on uek_acdemic_students (class_id)
+	comment '(null)';
+
+create index tg_user_students_fk
+	on uek_acdemic_students (user_id)
+	comment '(null)';
+
+-- 插入学生数据
+
+INSERT INTO eva.uek_acdemic_students (id, name, student_number, pro_team_id, status, gender, nation, phone, qq, email, idcard, member_id, user_id, class_id) VALUES (782, '张晋霞', '1713010101', 0, 2, null, null, null, null, null, null, null, 811, 1);
--- src/test/kotlin/com/nuc/evaluate/controller/UserControllerTest.kt
+++ src/test/kotlin/com/nuc/evaluate/controller/UserControllerTest.kt
@@ -1,5 +1,6 @@
 package com.nuc.evaluate.controller
 
+import com.alibaba.fastjson.JSON
 import com.nuc.evaluate.vo.User
 import org.junit.Before
 import org.junit.BeforeClass
@@ -7,9 +8,12 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.client.match.MockRestRequestMatchers
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 
@@ -30,7 +34,9 @@ class UserControllerTest {
 
     private lateinit var mockMvc: MockMvc
 
-
+    /**
+     * 参数初始化
+     */
     @Before
     fun initParams() {
         mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build()
@@ -41,7 +47,32 @@ class UserControllerTest {
      */
     @Test
     fun successLoginTest() {
-        val resultActions = mockMvc.perform(MockMvcRequestBuilders.post(LOGIN_URL))
-        println("result Action is ${resultActions.toString()}")
+        val SUCCESS_JSON = "{\n" +
+                "  \"data\": {\n" +
+                "    \"id\": 782,\n" +
+                "    \"name\": \"张晋霞\",\n" +
+                "    \"studentNumber\": \"1713010101\",\n" +
+                "    \"proTeamId\": 0,\n" +
+                "    \"status\": 2,\n" +
+                "    \"gender\": null,\n" +
+                "    \"nation\": null,\n" +
+                "    \"phone\": null,\n" +
+                "    \"qq\": null,\n" +
+                "    \"email\": null,\n" +
+                "    \"idcard\": null,\n" +
+                "    \"memberId\": null,\n" +
+                "    \"userId\": 811,\n" +
+                "    \"classId\": 1\n" +
+                "  },\n" +
+                "  \"code\": 200,\n" +
+                "  \"message\": \"登录成功\"\n" +
+                "}"
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post(LOGIN_URL)
+                .contentType(MediaType.APPLICATION_JSON_UTF8).content(JSON.toJSONString(userSuccess))
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.content().json(SUCCESS_JSON))
     }
 }
