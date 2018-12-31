fe9d1ddb08e8153864895847445d82127f6f23d0
youngxhui <youngxhui@gmail.com>
2018-12-31 17:32:51 +0800

    build(travis.yml): 添加启动配置文件
    使用 ci 配置文件启动

--- src/test/kotlin/com/nuc/evaluate/controller/UserControllerTest.kt
+++ src/test/kotlin/com/nuc/evaluate/controller/UserControllerTest.kt
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -46,7 +47,7 @@ private const val SUCCESS_JSON = """{
   "code": 200,
   "message": "登录成功"
 }"""
-
+@ActiveProfiles
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class UserControllerTest {
